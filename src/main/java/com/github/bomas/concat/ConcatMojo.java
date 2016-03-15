bc68e3cea2e6b80067b8977959976587c30003b2
Vardan Akopian <vakopian@sustain.net>
2016-03-15 14:29:38 -0700

    fix encoding issues

--- src/main/java/com/github/bomas/concat/ConcatMojo.java
+++ src/main/java/com/github/bomas/concat/ConcatMojo.java
@@ -20,6 +20,7 @@ import java.io.*;
 import java.util.List;
 
 import org.apache.commons.io.ByteOrderMark;
+import org.apache.commons.io.Charsets;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.BOMInputStream;
@@ -28,42 +29,42 @@ import org.apache.maven.plugin.MojoExecutionException;
 
 /**
  * Goal which concatenates several files and creates a new file as specified.
- * 
+ *
  * @Mojo( name = "concat" )
  * @goal concat
- * 
+ *
  * @Mojo( defaultPhase = "process-sources" )
  * @phase process-sources
  */
 public class ConcatMojo extends AbstractMojo {
-	
-	
+
+
 	/**
 	 * The resulting file
-	 * 
+	 *
 	 * @parameter
 	 * @required
 	 */
 	private File outputFile;
-	
-	
+
+
 	/**
 	 * Files to concatenate
-	 * 
+	 *
 	 * @parameter
 	 * @required
 	 */
 	private List<File> concatFiles;
-	
-	
+
+
 	/**
 	 * Append newline after each concatenation
-	 * 
+	 *
 	 * @parameter
 	 */
 	private boolean appendNewline=false;
 
-	
+
 	/**
 	 * Skip non-existent or unreadable files
 	 *
@@ -95,7 +96,7 @@ public class ConcatMojo extends AbstractMojo {
 		if(validate()){
 			getLog().info("Going to concatenate files to destination file: " + outputFile.getAbsolutePath());
 			try {
-                FileUtils.writeStringToFile(outputFile, "", false);
+                FileUtils.writeStringToFile(outputFile, "", "UTF-8", false);
 				for(File inputFile : concatFiles){
                     if (skipUnreadableFiles) {
                         if (!inputFile.exists()) {
@@ -107,24 +108,31 @@ public class ConcatMojo extends AbstractMojo {
                         }
                     }
 					getLog().debug("Concatenating file: " + inputFile.getAbsolutePath());
-                    String input;
+                    byte[] input;
                     try (InputStream inputStream = new FileInputStream(inputFile)) {
                         BOMInputStream bomInputStream = new BOMInputStream(inputStream);
                         ByteOrderMark bom = bomInputStream.getBOM();
-                        String charsetName = bom == null ? "UTF-8" : bom.getCharsetName();
-                        input = IOUtils.toString(bomInputStream, charsetName);
+                        final String charsetName;
+						if (bom == null) {
+							charsetName = "UTF-8";
+							getLog().info("input file " + inputFile.getAbsolutePath() + ": assuming charset " + charsetName);
+						} else {
+							charsetName = bom.getCharsetName();
+							getLog().info("input file " + inputFile.getAbsolutePath() + ": contains a BOM and has charset " + charsetName);
+						}
+						input = IOUtils.toByteArray(new InputStreamReader(bomInputStream, charsetName), "UTF-8");
                     }
                     if (fileHeader != null) {
                         String header = fileHeader.replace("{FILENAME}", inputFile.getName()).replace("{NL}", lineSeparator);
-                        FileUtils.writeStringToFile(outputFile, header, true);
+                        FileUtils.writeStringToFile(outputFile, header, "UTF-8", true);
                     }
-					FileUtils.writeStringToFile(outputFile, input, true);
+					FileUtils.writeByteArrayToFile(outputFile, input, true);
                     if (fileFooter != null) {
                         String footer = fileFooter.replace("{FILENAME}", inputFile.getName()).replace("{NL}", lineSeparator);
-                        FileUtils.writeStringToFile(outputFile, footer, true);
+                        FileUtils.writeStringToFile(outputFile, footer, "UTF-8", true);
                     }
 					if(appendNewline){
-                        FileUtils.writeStringToFile(outputFile, lineSeparator, true);
+                        FileUtils.writeStringToFile(outputFile, lineSeparator, "UTF-8", true);
 					}
 
 				}
@@ -133,12 +141,12 @@ public class ConcatMojo extends AbstractMojo {
 			}
 		}
 	}
-	
+
 	private boolean validate() throws MojoExecutionException{
 		if(outputFile == null){
 			throw new MojoExecutionException("Please specify a correct outPutFile");
 		}
-		
+
 		if(concatFiles == null || concatFiles.size() ==0){
 			throw new MojoExecutionException("Please specify the file(s) to concatenate");
 		}else if (!skipUnreadableFiles) {
@@ -147,9 +155,9 @@ public class ConcatMojo extends AbstractMojo {
 					throw new MojoExecutionException(file.getAbsolutePath() + " does not exists.");
 				}
 			}
-				
+
 		}
-		
+
 		return true;
 	}
 }
